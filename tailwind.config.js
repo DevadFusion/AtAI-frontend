4c0f7ea1f7f6260bc42cb529346b07f7f1322c10
DevadFusion <info@devadfusion.com>
2025-04-17 10:38:47 +0200

    small css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,jsx}"],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#2ECC71",
-          secondary: "#27AE60",
-          background: "#ECF0F1",
-        },
-        fontFamily: {
-          poppins: ["Poppins", "sans-serif"],
-        },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2ECC71",
+        secondary: "#27AE60",
+        background: "#ECF0F1",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
